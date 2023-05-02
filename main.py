ccc860877b5ae6f68071a527d1279cbffcb451b3
Gaurav Yadav <gauravyadav@betterhalf.ai>
2023-05-02 22:15:05 +0530

    Async-Await and calling async using eventloop

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+
+
+# async function
+async def my_fun(x, y):
+    return x + y
+
+
+# async function
+async def fun():
+    x = await my_fun(2, 3)  # calling an async function inside an async function
+    print(x)
+    x = x * x
+    print(x)
+
+
+# sync function
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(fun())
+    loop.close()
+
+
+main()
