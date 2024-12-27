d4b53a022ba79a2d3d08318092044b0cb027a1b0
gauravydv26 <gauravyadav@betterhalf.ai>
2024-12-27 14:39:11 +0530

    updated async code

--- sync_multithreading_parallelism.py
+++ sync_multithreading_parallelism.py
@@ -30,29 +30,29 @@ def fun3():
 
 async def async_fun1():
     start = datetime.now()
-    print("Starting fun1 Execution")
+    print("Starting async_fun1 Execution")
     time.sleep(3)
     await asyncio.sleep(2)
     time.sleep(1)
     end = datetime.now()
-    print(f"Time taken for execution of fun1 :: {(end - start).seconds} second(s)")
+    print(f"Time taken for execution of async_fun1 :: {(end - start).seconds} second(s)")
 
 
 async def async_fun2():
     start = datetime.now()
-    print("Starting fun2 Execution")
+    print("Starting async_fun2 Execution")
     await asyncio.sleep(2)
     end = datetime.now()
-    print(f"Time taken for execution of fun2 :: {(end - start).seconds} second(s)")
+    print(f"Time taken for execution of async_fun2 :: {(end - start).seconds} second(s)")
 
 
 async def async_fun3():
     start = datetime.now()
-    print("Starting fun3 Execution")
+    print("Starting async_fun3 Execution")
     time.sleep(1)
     await asyncio.sleep(2)
     end = datetime.now()
-    print(f"Time taken for execution of fun3 :: {(end - start).seconds} second(s)")
+    print(f"Time taken for execution of async_fun3 :: {(end - start).seconds} second(s)")
 
 
 def sync_calls():
@@ -72,6 +72,15 @@ async def async_calls():
     await asyncio.gather(*tasks)
     end = datetime.now()
     print(f"Time taken for async execution of fns :: {(end - start).seconds} second(s)")
+    """
+    1. async_fun1 is invoked
+    2. EL is blocked for 3 sec
+    3. EL hits await asyncio.sleep(2), and the EL starts executing the async_fun2
+    4. EL hits await asyncio.sleep(2), and the EL starts executing the async_fun3
+    5. EL is blocked for 1 sec, and EL hits await asyncio.sleep(2), and EL is now free for 1 sec.
+    6. EL comes to async_fun1 and get blocked for 1 sec and execution of async_fun1 is completed after EL get unblocked
+    7. EL completes the execution of async_fun2. and finally async_fun3 is executed by EL
+    """
 
 
 def multi_threaded():
@@ -83,19 +92,28 @@ def multi_threaded():
     t1.start()
     t2.start()
     t3.start()
+    """
+    here these three functions are expected to be
+    executed parallel but these would be executed concurrently due to GIL
+    and context switching would be done between threads.
+    """
     # t1.join()
     # t2.join()
     # t3.join()
+    """
+    thread.join is used to make the Main thread wait for
+    the completion of task a spawned thread is performing
+    """
     end = datetime.now()
     print(f"Time taken for multi-threaded execution of fns :: {(end - start).seconds} second(s)")
 
 
 def runner():
-    sync_calls()
+    # sync_calls()
     print("*********************************")
     asyncio.run(async_calls())
     print("*********************************")
-    multi_threaded()
+    # multi_threaded()
 
 
 if __name__ == "__main__":
